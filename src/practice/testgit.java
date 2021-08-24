c78d89e5dbf705b3c0519b742d659db908ec3b2b
hackerrank <me@hackerrank.com>
2021-08-24 23:12:09 +0530

    branch commit

--- src/practice/testgit.java
+++ src/practice/testgit.java
@@ -4,8 +4,9 @@ public class testgit {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Test");
+		System.out.println("updated");
 		// TODO Auto-generated method stub
+		System.out.println("git branch checking");
 
 	}
 
